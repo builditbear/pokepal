455dabc5ea029d9135f10a4c65febc86288dad1c
Brandon Chavez <brandon.na.chavez@pm.me>
2023-07-13 21:18:09 -0700

    Update async funcs to use tasks

--- app/components/pokedata-panel.js
+++ app/components/pokedata-panel.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-
+import { task, all } from 'ember-concurrency';
 export default class PokedataPanelComponent extends Component {
     @tracked pokemonData = [];
     @tracked searchQuery = '';
@@ -18,30 +18,59 @@ export default class PokedataPanelComponent extends Component {
         return randomPokemonIds;
     }
 
-    @action
-    async fetchPokemon(pokemonNameOrId) {
+    fetchPokemon = task(async (pokemonNameOrId) => {
         const response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${pokemonNameOrId}`
         );
         const data = await response.json();
         return data;
-    }
-    @action
-    async getRandomPokemonTeam(teamSize) {
+    });
+
+    getRandomPokemonTeam = task(async (teamSize) => {
         const pokemonTeam = [];
         const teamIds = this.generateRandomIds(teamSize);
 
-        await Promise.all(
+        await all(
             teamIds.map(async (id) => {
-                const pokemon = await this.fetchPokemon(id);
+                const pokemon = await this.fetchPokemon.perform(id);
                 pokemonTeam.push(pokemon);
             })
         );
 
         this.pokemonData = pokemonTeam;
-    }
-    @action
-    async searchPokemon() {
-        this.pokemonData = [await this.fetchPokemon(this.searchQuery)];
-    }
+    });
+
+    searchPokemon = task(async () => {
+        this.pokemonData = [await this.fetchPokemon.perform(this.searchQuery)];
+    })
+
+    // fetch API equivalents.
+    // @action
+    // async fetchPokemon(pokemonNameOrId) {
+    //     const response = await fetch(
+    //         `https://pokeapi.co/api/v2/pokemon/${pokemonNameOrId}`
+    //     );
+    //     const data = await response.json();
+    //     return data;
+    // }
+
+    // @action
+    // async getRandomPokemonTeam(teamSize) {
+    //     const pokemonTeam = [];
+    //     const teamIds = this.generateRandomIds(teamSize);
+
+    //     await Promise.all(
+    //         teamIds.map(async (id) => {
+    //             const pokemon = await this.fetchPokemon(id);
+    //             pokemonTeam.push(pokemon);
+    //         })
+    //     );
+
+    //     this.pokemonData = pokemonTeam;
+    // }
+
+    // @action
+    // async searchPokemon() {
+    //     this.pokemonData = [await this.fetchPokemon(this.searchQuery)];
+    // }
 }
