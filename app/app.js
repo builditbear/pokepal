f8b16b6eb73ea00385f0086273cdafd9ca12d748
Build-It Bear <brandon.na.chavez@pm.me>
2023-11-12 18:42:23 -0800

    Add glint for template aware ts linting

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-conc-demo/config/environment';
+import '@glint/environment-ember-loose';
+import '@glint/environment-ember-template-imports';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
